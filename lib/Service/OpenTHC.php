6fe4580c70f5ea17699a5d95ca40f8bf1d00f69a
openthc <code@openthc.com>
2020-02-12 19:34:30 +0000

    service adapter

---
+++ lib/Service/OpenTHC.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * OpenTHC Service Adapter
+ */
+
+namespace OpenTHC\Service;
+
+class OpenTHC
+{
+	private $_api_base = '';
+	private $_api_auth = '';
+	private $_ghc;
+	private $_raw;
+
+	function __construct($svc)
+	{
+		$cfg = \OpenTHC\Config::get(sprintf('openthc_%s', $svc));
+		$this->_api_base = $cfg['api_base'];
+		$this->_api_auth = $cfg['api_auth'];
+
+		$this->_ghc = new \GuzzleHttp\Client([
+			'base_uri' => $this->_api_base,
+			'headers' => [
+				'user-agent' => 'OpenTHC/420.20.040',
+				'authorization' => sprintf('Bearer %s', $this->_api_auth),
+			],
+			'http_errors' => false
+		]);
+	}
+
+	function get($url)
+	{
+		$res = $this->_ghc->get($url);
+		$this->_raw = $res->getBody()->getContents();
+		$ret = json_decode($this->_raw, true);
+		if (empty($ret['code'])) {
+			$ret['code'] = $res->getStatusCode();
+		}
+		return $ret;
+	}
+
+	function post($url, $arg)
+	{
+		$res = $this->_ghc->post($url, $arg);
+		$this->_raw = $res->getBody()->getContents();
+		$ret = json_decode($this->_raw, true);
+		if (empty($ret['code'])) {
+			$ret['code'] = $res->getStatusCode();
+		}
+		return $ret;
+	}
+}
