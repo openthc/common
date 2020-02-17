993743e2eb4927374901132702e36933ea3c9547
openthc <code@openthc.com>
2020-02-17 16:59:18 +0000

    session shut

---
+++ lib/Controller/Auth/Shut.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Close the Session
+ */
+
+namespace OpenTHC\Controller\Auth;
+
+class Shut extends \OpenTHC\Controller\Base
+{
+	function __invoke($REQ, $RES, $ARG)
+	{
+		$sid = session_id();
+		$snm = session_name();
+
+		if (!empty($sid)) {
+
+			$scp = session_get_cookie_params();
+
+			// Clear Session
+			session_unset();
+			session_gc();
+
+			// Clear Cookies?
+			//foreach ($_COOKIE as $k => $v) {
+			//	if ($k == $snm) {
+			//		setcookie($k, '', time() - 3600);
+			//	}
+			//}
+
+			// Rewrite this array key for PHP
+			$scp['expires'] = $scp['lifetime'];
+			unset($scp['lifetime']);
+			setcookie($snm, '', $scp);
+
+			session_regenerate_id(true);
+			session_destroy();
+			session_write_close();
+
+		}
+
+		return $RES->withJSON([
+			'meta' => [ 'detail' => 'Session Closed' ],
+			'data' => [],
+		]);
+	}
+}
