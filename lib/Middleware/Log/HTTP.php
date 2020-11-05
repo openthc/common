c6ccad45b6324cbc5329bd3c1f175c719f3c101a
openthc <code@openthc.com>
2020-11-05 18:23:35 +0000

    HTTP Logger

---
+++ lib/Middleware/Log/HTTP.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Log Request and Response
+ * @todo can we use GuzzleHttp\Psr7\str($req) ? 
+ */
+
+namespace OpenTHC\Middleware\Log;
+
+class HTTP extends \OpenTHC\Middleware\Base
+{
+	public function __invoke($REQ, $RES, $NMW)
+	{
+		// Log the Full Body
+		$log_file = sprintf('%s/var/log/%s.http', APP_ROOT, $_SERVER['UNIQUE_ID']);
+
+		$log_path = dirname($log_file);
+		if (!is_dir($log_path)) {
+			mkdir($log_path, 0755, true);
+		}
+
+		$log_pipe = fopen($log_file, 'a');
+
+		fwrite($log_pipe, json_encode(array(
+			'_GET' => $_GET,
+			'_POST' => $_POST,
+			'_COOKIE' => $_COOKIE,
+			'_SESSION' => $_SESSION,
+			'_SERVER' => $_SERVER,
+		)));
+		fwrite($log_pipe, "\n");
+		// file_put_contents($file, $data);
+
+		$RES = $NMW($REQ, $RES);
+
+		//$file = sprintf('%s/var/%s-res.dump', APP_ROOT, $_SERVER['UNIQUE_ID']);
+		// $file = '/tmp/res.dump';
+		$data = json_encode(array(
+			'_HEAD' => $RES->getHeaders(),
+			'_BODY' => $RES->getBody()->__toString(),
+		));
+		fwrite($log_pipe, $data);
+		fclose($log_pipe);
+		// file_put_contents($file, $data);
+
+		return $RES;
+
+	}
+}
