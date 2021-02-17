69e2636ebd7ad305690260a3170e0896b9bbf2f7
openthc <code@openthc.com>
2021-02-17 14:42:20 +0000

    fix key name

--- lib/Service/OpenTHC.php
+++ lib/Service/OpenTHC.php
@@ -14,7 +14,7 @@ class OpenTHC
 
 	function __construct($svc)
 	{
-		$cfg = \OpenTHC\Config::get(sprintf('%s', $svc));
+		$cfg = \OpenTHC\Config::get(sprintf('openthc_%s', $svc));
 		$this->_api_base = sprintf('https://%s/', $cfg['hostname']);
 		$this->_api_auth = $cfg['secret'];
 
