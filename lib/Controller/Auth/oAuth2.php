77ce48a4bb2820a4023d50b5c3b91a64f3b306b0
openthc <code@openthc.com>
2021-02-20 22:49:19 +0000

    getProvider implementations

--- lib/Controller/Auth/oAuth2.php
+++ lib/Controller/Auth/oAuth2.php
@@ -31,4 +31,46 @@ class oAuth2 extends \OpenTHC\Controller\Base
 
 	}
 
+	/**
+	 * Hard Coded Values for our SSO Service
+	 */
+	protected function getProvider($r=null)
+	{
+		$cfg = \OpenTHC\Config::get('openthc_sso');
+
+		$u = sprintf('https://%s/auth/back?%s', $_SERVER['SERVER_NAME'], http_build_query([ 'r' => $r ]));
+		$u = trim($u, '?');
+		$p = new \League\OAuth2\Client\Provider\GenericProvider([
+			'clientId' => ($cfg['public'] ?: $_SERVER['SERVER_NAME']),
+			'clientSecret' => $cfg['secret'],
+			'redirectUri' => $u,
+			'urlAuthorize' => sprintf('https://%s/oauth2/authorize', $cfg['hostname']),
+			'urlAccessToken' => sprintf('https://%s/oauth2/token', $cfg['hostname']),
+			'urlResourceOwnerDetails' => sprintf('https://%s/oauth2/profile', $cfg['hostname']),
+			'verify' => true
+		]);
+
+	}
+
+	/**
+	 * Return a Generic oAuth Provider
+	 */
+	protected function getProvider_Generic($r=null)
+	{
+		$cfg = \OpenTHC\Config::get('oauth');
+		if (empty($cfg)) {
+			throw new \Exception('OAuth2 must be configured [CAO-041]');
+		}
+
+		$u = sprintf('https://%s/auth/back?%s', $_SERVER['SERVER_NAME'], http_build_query(array('r' => $r)));
+		$u = trim($u, '?');
+
+		$cfg['redirectUri'] = $u;
+		$cfg['verify'] = true;
+
+		$p = new \League\OAuth2\Client\Provider\GenericProvider($cfg);
+
+		return $p;
+	}
+
 }
