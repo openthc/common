a17cc192c89fdd3ba3ee0a17be3c36f9474d01cd
openthc <code@openthc.com>
2021-09-02 22:46:35 +0000

    update flag and stat

--- lib/Contact.php
+++ lib/Contact.php
@@ -9,15 +9,16 @@ use Edoceo\Radix\DB\SQL;
 
 class Contact extends \OpenTHC\SQL\Record
 {
-	const TABLE = 'contact';
-
 	const FLAG_EMAIL_GOOD = 0x00000001;
-	const FlAG_PHONE_GOOD = 0x00000002;
+	const FLAG_PHONE_GOOD = 0x00000002;
 	const FLAG_EMAIL_WANT = 0x00000004;
 	const FLAG_PHONE_WANT = 0x00000008;
 
 	const FLAG_ROOT     = 0x00000010;
+	// const FLAG_PRIMARY  = 0x00000100; // Primary Contact ALT?
+
 	const FLAG_BILL     = 0x00000020;
+	// const FLAG_BILLING // ALT?
 
 	const FLAG_B2B_VENDOR = 0x00001000;
 	const FLAG_B2B_CLIENT = 0x00002000;
@@ -28,6 +29,12 @@ class Contact extends \OpenTHC\SQL\Record
 
 	const FLAG_MUTE     = 0x04000000;
 	const FLAG_DELETED  = 0x08000000;
+
+	const STAT_INIT = 100;
+	const STAT_LIVE = 200;
+	const STAT_GONE = 410;
+
+	const TABLE = 'contact';
 
 	protected $_table = 'contact';
 
