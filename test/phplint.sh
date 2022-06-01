def8972a379618670ad96936ee41ff64ccfd28e5
openthc <code@openthc.com>
2022-06-01 07:07:35 +0000

    common lint script

---
+++ test/phplint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# expects $OUTPUT_BASE, $OUTPUT_MAIN, $SOURCE_LIST to exist
+#
+
+set -o errexit
+set -o nounset
+
+declare OUTPUT_BASE
+declare OUTPUT_MAIN
+declare SOURCE_LIST
+
+if [ ! -f "$OUTPUT_BASE/phplint.txt" ]
+then
+
+	echo '<h1>Linting...</h1>' > "$OUTPUT_MAIN"
+
+	find "${code_list[@]}" -type f -name '*.php' -exec php -l {} \; \
+		| grep -v 'No syntax' \
+		>"$OUTPUT_BASE/phplint.txt" \
+		2>&1 \
+		|| true
+
+	[ -s "$OUTPUT_BASE/phplint.txt" ] || echo "Linting OK" >"$OUTPUT_BASE/phplint.txt"
+
+fi
