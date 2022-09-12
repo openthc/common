d099a60842ef6af9604bc1625656b459b26fe342
Matt Walther <matt@mashio.net>
2022-09-12 12:02:59 -0700

    Introduce addNote method to SQL record

--- lib/SQL/Record.php
+++ lib/SQL/Record.php
@@ -7,6 +7,8 @@ namespace OpenTHC\SQL;
 
 class Record implements \ArrayAccess, \JsonSerializable
 {
+    const FLAG_HAS_NOTE  = 0x00100000;
+
 	protected $_dbc; // An Edoceo\Radix\DB\SQL object
 
 	protected $_pk = null;
@@ -71,6 +73,55 @@ class Record implements \ArrayAccess, \JsonSerializable
 		}
 
 		$this->setData($obj);
+
+	}
+
+	/**
+	 * Add a Note to this Object
+	 * @param string $note [description]
+	 * @param bool pin to top
+	 */
+	function addNote($note, $pin=0)
+	{
+		$link = sprintf('%s:%s', $this->_table, $this->_data['id']);
+		$pin = intval($pin);
+
+		// SQL::query('BEGIN');
+		$this->_dbc->query('BEGIN');
+
+		if (!empty($pin)) {
+
+			$pin = 1;
+
+			// Update all other Notes on this Object to be !PIN
+			$sql = 'UPDATE object_note SET flag = (flag & ~ :f0::int) WHERE link = :l0';
+			$arg = array(
+				':l0' => $link,
+				':f0' => $pin,
+			);
+			// SQL::query($sql, $arg);
+			$this->_dbc->query($sql, $arg);
+
+		}
+
+		// Add Note
+		$sql = 'INSERT INTO object_note (auth_contact_id, flag, link, note) VALUES (?, ?, ?, ?)';
+		$arg = array($_SESSION['Contact']['id'], $pin, $link, $note);
+		// $res = SQL::query($sql, $arg);
+		$res = $this->_dbc->query($sql, $arg);
+
+		// Tag Object
+		$sql = sprintf('UPDATE %s SET flag = flag | :f1 WHERE id = :id', $this->_table);
+		$arg = array(':f1' => self::FLAG_HAS_NOTE, ':id' => $this->_data['id']);
+		// SQL::query($sql, $arg);
+		$this->_dbc->query($sql, $arg);
+
+		// SQL::query('COMMIT');
+		$this->_dbc->query('COMMIT');
+
+		$this->_data['flag'] = ($this->_data['flag'] | self::FLAG_HAS_NOTE);
+
+		return $res;
 
 	}
 
