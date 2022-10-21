bd6089ccfc6f7e8d5ceb2b5988dc8ffcdd6ef8a6
openthc <code@openthc.com>
2022-10-21 19:02:14 +0000

    add CORS middleware

---
+++ lib/Middleware/CORS.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * CORS Headers
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace OpenTHC\Middleware;
+
+class CORS extends \OpenTHC\Middleware\Base
+{
+	/**
+	 *
+	 */
+	public function __invoke($REQ, $RES, $NMW)
+	{
+		// no origin? pass-thru
+		if (empty($_SERVER['HTTP_ORIGIN'])) {
+			return $NMW($REQ, $RES);
+		}
+
+		$RES = $RES->withHeader('access-control-allow-headers', 'Authorization')
+			->withHeader('access-control-allow-methods', 'GET,POST,PUT')
+			->withHeader('access-control-max-age', 86400)
+			//->withHeader('content-type', 'text/plain')
+			->withHeader('vary', 'accept-encoding');
+
+		// withHeader('access-control-allow-credentials', 'true')
+
+		// No Response on OPTIONS
+		if ($REQ->isOptions()) {
+			return $RES->withHeader('content-length', '0')
+				->withHeader('content-type', 'text/plain');
+		}
+
+		// @todo lookup origin, if we've got a whitelist
+		$o = strtolower($_SERVER['HTTP_ORIGIN']);
+		$cfg = \OpenTHC\Config::get('cors_origin');
+		if (!empty($cfg)) {
+			$key_list = array_keys($cfg);
+			if (!in_array('*', $key_list)) { // no star
+				if (!in_array($o, $key_list)) { // no host
+					return $RES->withStatus(403);
+				}
+			}
+		}
+
+		// Allow this Origin
+		$RES = $RES->withHeader('access-control-allow-origin', $o);
+
+
+		return $NMW($REQ, $RES);
+
+	}
+}
