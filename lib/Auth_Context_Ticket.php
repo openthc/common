5d251c1f678f80b5e86303251f2678605ef67fba
Matt Walther <matt@mashio.net>
2023-02-24 16:30:22 -0800

    import components from sso app#792

---
+++ lib/Auth_Context_Ticket.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * An Authentication Context Ticket
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace OpenTHC;
+
+class Auth_Context_Ticket extends \OpenTHC\SQL\Record
+{
+	protected $_table = 'auth_context_ticket';
+
+	function create($d)
+	{
+		$rec = [];
+		$rec['id'] = _random_hash();
+
+		if (!empty($d['expires_at'])) {
+			$rec['expires_at'] = $d['expires_at'];
+			unset($d['expires_at']);
+		}
+
+		$rec['meta'] = json_encode($d);
+
+		$this->_pk = $this->_dbc->insert($this->_table, $rec);
+		$this->_data['id'] = $this->_pk;
+
+	}
+
+	function isValid()
+	{
+		// @todo Check the expires_at Time
+		$dt0 = new \DateTime($this->_data['expires_at']);
+		$dt1 = new \DateTime();
+
+	}
+
+}
---
+++ lib/CSRF.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Handles CSRF / DUR Tokens
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace OpenTHC;
+
+class CSRF
+{
+	const MAX_LIFE = 600;
+	const MIN_LIFE = 60;
+
+	/**
+	 *
+	 */
+	static function init() : void
+	{
+		if (empty($_SESSION['_csrf'])) {
+			$_SESSION['_csrf'] = [];
+		} elseif (!is_array($_SESSION['_csrf'])) {
+			$_SESSION['_csrf'] = [];
+		}
+
+	}
+
+	/**
+	 *
+	 */
+	static function getToken() : string
+	{
+		self::init();
+
+		$ret = null;
+		$key_list = array_keys($_SESSION['_csrf']);
+		foreach ($key_list as $key) {
+
+			$tok = $_SESSION['_csrf'][$key];
+			$age = $tok['expires_at'] - $_SERVER['REQUEST_TIME'];
+
+			// Positive Age is Valid
+			if ($age > 0) {
+				$ret = $key;
+				break;
+			} else {
+				// Invalid
+				unset($_SESSION['_csrf'][$key]);
+			}
+
+		}
+
+		// Create a Token (if needed)
+		if (empty($ret)) {
+			$ret = _random_hash();
+			$_SESSION['_csrf'][$ret] = [
+				'created_at' => $_SERVER['REQUEST_TIME'],
+				'expires_at' => ($_SERVER['REQUEST_TIME'] + self::MAX_LIFE),
+			];
+		}
+
+		return $ret;
+
+	}
+
+	/**
+	 * @param string $key -- the key to lookup
+	 * @param bool $and_clear -- set to true to expire the key
+	 */
+	static function verify(string $key, bool $and_clear=false) : bool
+	{
+		self::init();
+
+		if (empty($key)) {
+			_exit_html_warn('<h1>Invalid Request [ALC-072]</h1><h2><a href="javascript:history.back();">go back</a></h2>', 400);
+			return false;
+		}
+
+		$tok = $_SESSION['_csrf'][$key];
+		if (empty($tok)) {
+			_exit_html_warn('<h1>Invalid Request [ALC-077]</h1><h2><a href="javascript:history.back();">go back</a></h2>', 400);
+			return false;
+		}
+
+		$age = $tok['expires_at'] - $_SERVER['REQUEST_TIME'];
+		if ($age > 0) {
+			if ($and_clear) {
+				unset($_SESSION['_csrf'][$key]);
+			}
+			return true;
+		}
+
+		__exit_text('Invalid Request [ALC-091]', 400);
+		return false;
+	}
+
+}
