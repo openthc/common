7fba038b49eb2bd3a41cbd694297dbe69e1fef9e
openthc <code@openthc.com>
2023-05-01 13:01:12 +0000

    improve Auth_Context_Ticket

--- lib/Auth_Context_Ticket.php
+++ lib/Auth_Context_Ticket.php
@@ -11,6 +11,9 @@ class Auth_Context_Ticket extends \OpenTHC\SQL\Record
 {
 	protected $_table = 'auth_context_ticket';
 
+	/**
+	 *
+	 */
 	function create($d)
 	{
 		$rec = [];
@@ -26,13 +29,33 @@ class Auth_Context_Ticket extends \OpenTHC\SQL\Record
 		$this->_pk = $this->_dbc->insert($this->_table, $rec);
 		$this->_data['id'] = $this->_pk;
 
+		return $rec['id'];
+
 	}
 
+	/**
+	 * Determine if the Auth_Context_Ticket is valid
+	 */
 	function isValid()
 	{
+		$val = $this->_data['meta'];
+		if (empty($val)) {
+			return false;
+		}
+
+		$val = json_decode($val, true);
+		if (empty($val)) {
+			return false;
+		}
+
 		// @todo Check the expires_at Time
 		$dt0 = new \DateTime($this->_data['expires_at']);
 		$dt1 = new \DateTime();
+		if ($dt0 < $dt1) {
+			return false;
+		}
+
+		return true;
 
 	}
 
