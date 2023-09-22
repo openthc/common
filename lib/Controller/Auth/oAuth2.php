dfc478d3ede927e4d7f6beb763ad0fe19069608d
openthc <code@openthc.com>
2023-09-22 20:33:44 +0000

    improve method to load oauth client via config

--- lib/Controller/Auth/oAuth2.php
+++ lib/Controller/Auth/oAuth2.php
@@ -36,17 +36,20 @@ class oAuth2 extends \OpenTHC\Controller\Base
 	 */
 	protected function getProvider($ret=null)
 	{
+		$client_cfg = [
+			'clientId' => '',
+			'clientSecret' => '',
+			'redirectUri' => '',
+			'urlAccessToken' => '',
+			'urlAuthorize' => '',
+			'urlResourceOwnerDetails' => '',
+			'verify' => true
+		];
+
 		$cfg = \OpenTHC\Config::get('openthc/sso');
-		$cfg['client_id'] = $cfg['public'] ?: $cfg['id'];
-		$cfg['client_sk'] = $cfg['secret'];
 
-		if (empty($cfg['client_id'])) {
-			throw new \Exception('Invalid Client Configuration [CAO-040]');
-		}
-		if (empty($cfg['client_sk'])) {
-			throw new \Exception('Invalid Client Configuration [CAO-043]');
-		}
-
+		// Find the oAuth Server Origin
+		// From from two deprecated ways
 		$sso_origin = $cfg['origin'];
 		if (empty($sso_origin)) {
 			$sso_origin = $cfg['base'];
@@ -56,17 +59,28 @@ class oAuth2 extends \OpenTHC\Controller\Base
 		}
 		$sso_origin = rtrim($sso_origin, '/');
 
+		$client_cfg['urlAccessToken'] = sprintf('%s/oauth2/token', $sso_origin);
+		$client_cfg['urlAuthorize'] = sprintf('%s/oauth2/authorize', $sso_origin);
+		$client_cfg['urlResourceOwnerDetails'] = sprintf('%s/oauth2/profile', $sso_origin);
+
+		// Auth Keys
+		// handles deprecated methods here too
+		$client_cfg['clientId'] = $cfg['oauth-client-id'] ?: $cfg['client-id'] ?: $cfg['public'] ?: $cfg['id'];
+		$client_cfg['clientSecret'] = $cfg['oauth-client-sk'] ?: $cfg['client-sk'] ?: $cfg['secret'];
+
+		if (empty($client_cfg['clientId'])) {
+			throw new \Exception('Invalid Client Configuration [CAO-040]');
+		}
+		if (empty($client_cfg['clientSecret'])) {
+			throw new \Exception('Invalid Client Configuration [CAO-043]');
+		}
+
 		$url = sprintf('%s/auth/back?%s', OPENTHC_SERVICE_ORIGIN, http_build_query([ 'r' => $ret ]));
 		$url = trim($url, '?');
-		$loc = new \League\OAuth2\Client\Provider\GenericProvider([
-			'clientId' => $cfg['client_id'],
-			'clientSecret' => $cfg['client_sk'],
-			'redirectUri' => $url,
-			'urlAuthorize' => sprintf('%s/oauth2/authorize', $sso_origin),
-			'urlAccessToken' => sprintf('%s/oauth2/token', $sso_origin),
-			'urlResourceOwnerDetails' => sprintf('%s/oauth2/profile', $sso_origin),
-			'verify' => true
-		]);
+
+		$client_cfg['redirectUri'] = $url;
+
+		$loc = new \League\OAuth2\Client\Provider\GenericProvider($client_cfg);
 
 		return $loc;
 
