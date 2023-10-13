de2475cd49425aa01ad6c887d1654e3f09599706
openthc <code@openthc.com>
2023-10-13 16:57:47 +0000

    do not use cached output anymore

--- test/phpunit.sh
+++ test/phpunit.sh
@@ -12,28 +12,22 @@ declare OUTPUT_MAIN
 
 #
 # PHPUnit
-if [ ! -f "$OUTPUT_BASE/phpunit.html" ]
-then
+xsl_file="test/phpunit.xsl"
 
-	xsl_file="test/phpunit.xsl"
-
-	echo '<h1>PHPUnit...</h1>' > "$OUTPUT_MAIN"
-
-	vendor/bin/phpunit \
-		--configuration "test/phpunit.xml" \
-		--log-junit "$OUTPUT_BASE/phpunit.xml" \
-		--testdox-html "$OUTPUT_BASE/testdox.html" \
-		--testdox-text "$OUTPUT_BASE/testdox.txt" \
-		--testdox-xml "$OUTPUT_BASE/testdox.xml" \
-		"$@" 2>&1 | tee "$OUTPUT_BASE/phpunit.txt"
+echo '<h1>PHPUnit...</h1>' > "$OUTPUT_MAIN"
+vendor/bin/phpunit \
+	--configuration "test/phpunit.xml" \
+	--log-junit "$OUTPUT_BASE/phpunit.xml" \
+	--testdox-html "$OUTPUT_BASE/testdox.html" \
+	--testdox-text "$OUTPUT_BASE/testdox.txt" \
+	--testdox-xml "$OUTPUT_BASE/testdox.xml" \
+	"$@" 2>&1 | tee "$OUTPUT_BASE/phpunit.txt"
 
 
-	[ -f "$xsl_file" ] || curl -qs 'https://openthc.com/pub/phpunit/report.xsl' > "$xsl_file"
+[ -f "$xsl_file" ] || curl -qs 'https://openthc.com/pub/phpunit/report.xsl' > "$xsl_file"
 
-	xsltproc \
-		--nomkdir \
-		--output "$OUTPUT_BASE/phpunit.html" \
-		"$xsl_file" \
-		"$OUTPUT_BASE/phpunit.xml"
-
-fi
+xsltproc \
+	--nomkdir \
+	--output "$OUTPUT_BASE/phpunit.html" \
+	"$xsl_file" \
+	"$OUTPUT_BASE/phpunit.xml"
