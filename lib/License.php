cffa9cf4baaa072cd38370972802fa7510a57ae9
openthc <code@openthc.com>
2023-12-01 22:22:41 +0000

    update Icon w/Tooltip

--- lib/License.php
+++ lib/License.php
@@ -111,19 +111,23 @@ class License extends \OpenTHC\SQL\Record
 		switch ($this->_data['stat'])
 		{
 			case 100:
-				$icon[] = '<i class="text-secondary fa-regular fa-square-plus"></i>';
+				$icon[] = '<i class="text-secondary fa-regular fa-square-plus" title="New / Pending"></i>';
 				break;
 			case 102:
-				$icon[] = '<i class="text-secondary fa-regular fa-circle-question"></i>';
+				$icon[] = '<i class="text-secondary fa-regular fa-circle-question" title="Processing"></i>';
 				break;
 			case 200:
-				$icon[] = '<i class="text-success fa-regular fa-square-check"></i>';
+				$icon[] = '<i class="text-success fa-regular fa-square-check" title="Active"></i>';
 				break;
 			case 308:
-				$icon[] = '<i class="text-warning fa-solid fa-arrows-left-right"></i>';
+				$icon[] = '<i class="text-warning fa-solid fa-arrows-left-right" title="Moved"></i>';
 				break;
+			case 404:
 			case 410:
-				$icon[] = '<i class="text-danger fa-solid fa-ban"></i>';
+				$icon[] = '<i class="text-danger fa-solid fa-ban" title="Closed"></i>';
+				break;
+			case 451:
+				$icon[] = '<i class="fa-solid fa-building-lock" title="Blocked / Locked"></i>';
 				break;
 			default:
 				$icon[] = sprintf('%d', $v['stat']);
