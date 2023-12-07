054d6f94c3c2183f663a88534b8e683a23d6f88f
openthc <code@openthc.com>
2023-12-07 15:56:30 +0000

    add GeoIP

---
+++ lib/GeoIP.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Simple Redis Based Cache
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace OpenTHC;
+
+class GeoIP
+{
+	private $api;
+
+	function __construct($cfg=null)
+	{
+		if (empty($cfg)) {
+			$cfg = \OpenTHC\Config::get('maxmind');
+		}
+
+		if ( ! empty($cfg['account'])) {
+			$this->api = new \GeoIp2\WebService\Client($cfg['account'], $cfg['license-key']);
+		}
+	}
+
+	/**
+	 * @return array data of GeoIP Lookup
+	 */
+	function get(string $ip)
+	{
+		if ( ! empty($this->api)) {
+			$res = $this->api->city($ip);
+			$ret = $res->raw;
+			return $ret;
+		}
+
+		// Fallback
+		// Return an array that looks kinda like the Maxmind GeoIP one.
+		if (function_exists('geoip_record_by_name')) {
+			$res = geoip_record_by_name($ip);
+			$ret = [
+				'city' => [
+					'name' => $res['city'],
+				],
+				'continent' => [
+					'code' => $res['continent_code'],
+				],
+				'country' => [
+					'iso_code' => $res['country_code'],
+				],
+				'location' => [
+					'latitude' => $res['latitude'],
+					'longitude' => $res['longitude'],
+				],
+				'postal' => [
+					'code' => $res['postal_code'],
+				]
+			];
+			return $ret;
+		}
+
+		return [];
+
+	}
+
+}
