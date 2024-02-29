2c583c744c1dca9f94d20fc3fbdc23b95beb8ea4
openthc <code@openthc.com>
2024-02-29 20:41:00 +0000

    PUT wrapper, short timeout

--- lib/OPA.php
+++ lib/OPA.php
@@ -11,6 +11,8 @@ namespace OpenTHC;
 
 class OPA
 {
+	private $_url = '';
+
 	/**
 	 *
 	 */
@@ -54,30 +56,15 @@ class OPA
 	/**
 	 *
 	 */
-	function data_set($path, $data)
+	function data_set(string $path, $data)
 	{
 		$url = sprintf('%s/v1/data/%s', $this->_url, $path);
-		$req = _curl_init($url);
 
-		// POST JSON
-		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'PUT');
-		curl_setopt($req, CURLOPT_POSTFIELDS, json_encode($data));
-		curl_setopt($req, CURLOPT_HTTPHEADER, [
-			'content-type: application/json'
-		]);
-		$res = curl_exec($req);
-		$inf = curl_getinfo($req);
-
-		switch ($inf['http_code']) {
-			case 200:
-			case 204:
-				// OK
-				break;
-			default:
-				var_dump($inf);
-				throw new \Exception('Invalid Response from OPA [OLO-050]');
+		if (is_array($data) || is_object($data)) {
+			$data = json_encode($data);
 		}
 
+		$res = $this->_url_put($url, $data, 'application/json');
 		$res = json_decode($res);
 
 		return $res;
@@ -87,18 +74,24 @@ class OPA
 	/**
 	 *
 	 */
-	function policy_get($path, $ctx)
+	function policy_get(string $path, $ctx)
 	{
 		$url = sprintf('%s/v1/data/%s', $this->_url, $path);
 		$req = _curl_init($url);
 		// POST JSON
 		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'POST');
+		// curl_setopt($req, CURLOPT_CONNECTTIMEOUT, 4); // Allowed Connect Time (s)
+		// curl_setopt($req, CURLOPT_CONNECTTIMEOUT_MS, 4000); // Allowed Connect Time (ms)
+		curl_setopt($req, CURLOPT_TIMEOUT, 4); // Allowed Total Time (s)
+		// curl_setopt($req, CURLOPT_TIMEOUT_MS, 8000); // Allowed Total Time (ms)
 		curl_setopt($req, CURLOPT_POSTFIELDS, json_encode([ 'input' => $ctx ]));
 		curl_setopt($req, CURLOPT_HTTPHEADER, [
 			'content-type: application/json'
 		]);
+
 		$res = curl_exec($req);
 		$res = json_decode($res);
+
 		if (empty($res)) {
 			return new \stdClass();
 		}
@@ -114,7 +107,7 @@ class OPA
 	/**
 	 *
 	 */
-	function policy_set($rego)
+	function policy_set(string $rego)
 	{
 		if ( ! preg_match('/^package (.+)$/m', $rego, $m)) {
 			throw new \Exception('Invalid REGO [OLO-053]');
@@ -122,14 +115,7 @@ class OPA
 		$path = str_replace('.', '/', $m[1]);
 
 		$url = sprintf('%s/v1/policies/%s', $this->_url, $path);
-		$req = _curl_init($url);
-		// POST TEXT
-		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'PUT');
-		curl_setopt($req, CURLOPT_POSTFIELDS, $rego);
-		curl_setopt($req, CURLOPT_HTTPHEADER, [
-			'content-type: text/plain'
-		]);
-		$res = curl_exec($req);
+		$res = $this->_curl_put($url, $rego, 'text/plain');
 		$res = json_decode($res);
 
 		return $res;
@@ -138,6 +124,37 @@ class OPA
 
 	function query()
 	{
+
+	}
+
+	/**
+	 * PUT Wrapper
+	 */
+	function _curl_put(string $url, string $data, string $type)
+	{
+		$req = _curl_init($url);
+		// POST TEXT
+		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'PUT');
+		curl_setopt($req, CURLOPT_TIMEOUT, 8); // Allowed Total Time (s)
+		curl_setopt($req, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($req, CURLOPT_HTTPHEADER, [
+			sprintf('content-type: %s', $type)
+		]);
+
+		$res = curl_exec($req);
+		$inf = curl_getinfo($req);
+
+		switch ($inf['http_code']) {
+			case 200:
+			case 204:
+				// OK
+				break;
+			default:
+				var_dump($inf);
+				throw new \Exception('Invalid Response from OPA [OLO-149]');
+		}
+
+		return $res;
 
 	}
 
