f0f50316a79655c02e4f88344d12807188318053
openthc <code@openthc.com>
2024-03-21 22:16:39 +0000

    improve 404 handler

--- lib/App.php
+++ lib/App.php
@@ -39,14 +39,64 @@ class App extends \Slim\App
 			$con = $this->addTwig($con);
 		}
 
-		//
+		// Not Found Helper
 		$con['notFoundHandler'] = function($c) {
-			return function ($REQ, $RES) {
-				return $RES->withJSON([
-					'data' => null,
-					'meta' => [ 'note' => 'Not Found [CLA-037]' ]
-				], 404);
+			return new class extends \Slim\Handlers\NotFound {
+
+				private $e;
+
+				public function __invoke($request, $response, $e=null)
+				{
+					$this->e = $e;
+					return parent::__invoke($request, $response);
+				}
+
+				function renderHtmlNotFoundOutput($request)
+				{
+					return <<<HTML
+					<!DOCTYPE html>
+					<html lang="en">
+					<head>
+					<meta charset="utf-8">
+					<meta name="viewport" content="initial-scale=1, user-scalable=yes, width=device-width">
+					<meta name="application-name" content="OpenTHC">
+					<title>Not Found</title>
+					<style>
+					body {
+						margin:0;
+						padding: 4vh 4vw;
+						font:1.5rem/1.5 Helvetica,Arial,Verdana,sans-serif;
+					}
+					h1 {
+						margin:0;
+						font-size: 3rem;
+						font-weight: normal;
+						line-height: 3rem;
+					}
+					</style>
+					</head>
+					<body>
+						<h1>Not Found</h1>
+						<p>{$this->e->note}</p>
+					</body>
+					</html>
+					HTML;
+
+					// $html = parent::renderHtmlNotFoundOutput($request);
+					// $this->request->getAttribute('note')
+					// return $html;<!DOCTYPE html>
+				}
+
+				function renderJsonNotFoundOutput()
+				{
+					return json_encode([
+						'data' => 'null',
+						'meta' => [ 'note' => $this->e->note ],
+					]);
+				}
+
 			};
+
 		};
 
 		return parent::__construct($con);
