3c968373ee8bf354a1cbb53546030b685e8631f2
openthc <code@openthc.com>
2024-04-02 04:10:33 +0000

    formatting

--- lib/Service/Ntfy.php
+++ lib/Service/Ntfy.php
@@ -25,19 +25,22 @@ class Ntfy
 		$this->ch = ltrim($ch, '/');
 	}
 
-	function send(string $msg, ?int $pri)
+	/**
+	 *
+	 */
+	function send(string $msg, int $pri=0)
 	{
 		$req = _curl_init(sprintf('https://ntfy.sh/%s', $this->ch));
 		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'POST');
 		curl_setopt($req, CURLOPT_POSTFIELDS, $msg);
 		curl_setopt($req, CURLOPT_HTTPHEADER, [
-		        'content-type: text/plain',
+			'content-type: text/plain',
 			// 'title' => '',
 			// 'priority' => '',
 			// 'tags' => '',
 		]);
 
-		// curl_setopt($req, 
+		// curl_setopt($req,
 
 		$res = curl_exec($req);
 		return json_decode($res);
