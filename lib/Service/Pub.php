63c932f50c4c4677b2944a2b7d379c1d1db367f9
openthc <code@openthc.com>
2024-04-05 21:09:16 +0000

    improve Pub put

--- lib/Service/Pub.php
+++ lib/Service/Pub.php
@@ -21,8 +21,6 @@ class Pub
 
 	private $sk;
 
-	public $msg;
-
 	/**
 	 *
 	 */
@@ -38,50 +36,35 @@ class Pub
 		$this->server_origin = \OpenTHC\Config::get('openthc/pub/origin');
 		$this->server_pk = \OpenTHC\Config::get('openthc/pub/public');
 
-		$this->msg = [
-			'path' => '',
-			'name' => '',
-		];
-
 	}
 
 	/**
 	 * Set the Paths to Write to for the PUT operation
 	 */
-	function setPath(string $path) : string {
+	private function getPath(string $path) : string {
 
 		$tmp_name = basename($path);
 		$tmp_path = dirname($path);
-		// Check Them ('./a nd stuff')
 
-		if ($this->msg['path'] != $tmp_path) {
+		// Create Predictable Location
+		$hkey = sodium_crypto_generichash($this->client_sk, '', SODIUM_CRYPTO_GENERICHASH_KEYBYTES);
+		$seed = sodium_crypto_generichash($tmp_path, $hkey, SODIUM_CRYPTO_GENERICHASH_KEYBYTES);
+		$kp0 = sodium_crypto_box_seed_keypair($seed);
+		$this->pk = sodium_crypto_box_publickey($kp0);
+		$this->sk = sodium_crypto_box_secretkey($kp0);
 
-			$this->msg['path'] = $tmp_path;
+		$new_path = Sodium::b64encode($this->pk);
 
-			// Create Predictable Location
-			$hkey = sodium_crypto_generichash($this->client_sk, '', SODIUM_CRYPTO_GENERICHASH_KEYBYTES);
-			$seed = sodium_crypto_generichash($this->msg['path'], $hkey, SODIUM_CRYPTO_GENERICHASH_KEYBYTES);
-			$kp0 = sodium_crypto_box_seed_keypair($seed);
-			$this->pk = sodium_crypto_box_publickey($kp0);
-			$this->sk = sodium_crypto_box_secretkey($kp0);
-
-			$this->req_path_base = Sodium::b64encode($this->pk);
-		}
-
-		$this->msg['name'] = $tmp_name;
-
-		return $this->getURL();
+		return sprintf('/%s/%s', $new_path, $tmp_name);
 
 	}
 
 	/**
 	 *
 	 */
-	function getUrl() : string {
+	function getUrl(string $path) : string {
 
-		$req_path = sprintf('%s/%s', $this->req_path_base, $this->msg['name']);
-		$url_full = sprintf('%s/%s', $this->server_origin, $req_path);
-
+		$url_full = sprintf('%s%s', $this->server_origin, $this->getPath($path));
 		return $url_full;
 
 	}
@@ -89,13 +72,15 @@ class Pub
 	/**
 	 *
 	 */
-	function get($path) : array {
+	function get(string $path) : array {
 
 		$url = $path;
 		if ( ! preg_match('/^http.+/', $url)) {
 			$path = ltrim($path, '/');
 			$url = sprintf('%s/%s', $this->server_origin, $path);
 		}
+
+		// Re-Calc?
 
 		// GET
 		$req = _curl_init($url);
@@ -117,9 +102,9 @@ class Pub
 
 	}
 
-	function put($body, $type) : array {
+	function put(string $path, $body, string $type) : array {
 
-		$msg = $this->msg;
+		$msg = [];
 
 		$msg['type'] = $type;
 
@@ -134,7 +119,7 @@ class Pub
 		$req_auth = Sodium::encrypt($req_auth, $this->client_sk, $this->server_pk);
 		$req_auth = Sodium::b64encode($req_auth);
 
-		$url = $this->getUrl();
+		$url = $this->getUrl($path);
 		$req = _curl_init($url);
 		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'POST');
 		curl_setopt($req, CURLOPT_POSTFIELDS, $body);
