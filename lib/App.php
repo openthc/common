d9dba321c074fbfa1eb43d6c501b7447c38a362a
openthc <code@openthc.com>
2024-09-11 19:20:02 +0000

    drop twig helpers

--- lib/App.php
+++ lib/App.php
@@ -30,15 +30,6 @@ class App extends \Slim\App
 		// Update App Container
 		$con = new \Slim\Container($this->__cfg);
 
-		// Default Twig Directory, or Specific Option?
-		$this->__cfg['twig'] = APP_ROOT . '/twig';
-		if (!empty($opt['twig'])) {
-			$this->__cfg['twig'] = $opt['twig'];
-		}
-		if (is_dir($this->__cfg['twig'])) {
-			$con = $this->addTwig($con);
-		}
-
 		// Not Found Helper
 		$con['notFoundHandler'] = function($c) {
 			return new class extends \Slim\Handlers\NotFound {
@@ -100,49 +91,6 @@ class App extends \Slim\App
 		};
 
 		return parent::__construct($con);
-
-	}
-
-	function addTwig($con)
-	{
-		// Load Slim View
-		$con['view'] = function($c0) {
-
-			// Add Common to Twig Path
-			$path = array(
-				$this->__cfg['twig'],
-				sprintf('%s/twig', dirname(dirname(__FILE__))),
-			);
-
-			$args = array(
-				//'cache' => '/tmp',
-				'debug' => $this->__cfg['debug'],
-			);
-
-			$view = new \Slim\Views\Twig($path, $args);
-
-			if ($this->__cfg['debug']) {
-				$view->addExtension(new \Twig\Extension\DebugExtension());
-			}
-
-			// Base64 Filter (for Email)
-			$tfb = new \Twig\TwigFilter('base64', function($x) {
-				return chunk_split(base64_encode($x), 72);
-			});
-			$view->getEnvironment()->addFilter($tfb);
-
-			// Markdown Filter
-			$tfm = new \Twig\TwigFilter('markdown', function($x) {
-				return _markdown($x);
-			}, array('is_safe' => array('html')));
-
-			$view->getEnvironment()->addFilter($tfm);
-
-			return $view;
-
-		};
-
-		return $con;
 
 	}
 
