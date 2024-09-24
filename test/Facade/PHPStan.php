add175f469ed548580c80f8c0118f73a801c7c39
openthc <code@openthc.com>
2024-09-24 02:36:04 +0000

    fix path adder

--- test/Facade/PHPStan.php
+++ test/Facade/PHPStan.php
@@ -50,23 +50,39 @@ class PHPStan {
 		// $stan_config = yaml_emit($cfg);
 
 		// It's not YAML it's NEON
-		$stan_config = <<<CFG
-		parameters:
-			bootstrapFiles:
-				- ../boot.php
-			level: 9
-			paths:
-				- ../bin
-				- ../etc
-				- ../lib
-				- ../sbin
-				- ../test
-				- ../view
-			ignoreErrors:
-				- '/Undefined variable: \\\$this/'
-				- '/Using \\\$this outside a class/'
-				- '/Variable \\\$this might not be defined/'
-		CFG;
+		$stan_config = [];
+		$stan_config[] = 'parameters:';
+		$stan_config[] = '	bootstrapFiles:';
+		$stan_config[] = '		- ../boot.php';
+		if (is_file(APP_ROOT . '/lib/autoload.php')) {
+			$stan_config[] = '		- ../lib/autoload.php';
+		}
+		if (is_file(APP_ROOT . '/lib/cli.php')) {
+			$stan_config[] = '		- ../lib/cli.php';
+		}
+		if (is_file(APP_ROOT . '/lib/php.php')) {
+			$stan_config[] = '		- ../lib/php.php';
+		}
+		if (is_file(APP_ROOT . '/lib/web.php')) {
+			$stan_config[] = '		- ../lib/web.php';
+		}
+		$stan_config[] = '	level: 9';
+		$stan_config[] = '	paths:';
+		$path_list = [ 'bin', 'block', 'controller', 'etc', 'lib', 'sbin', 'test', 'theme', 'view' ];
+		foreach ($path_list as $p) {
+			if (is_dir(sprintf('%s/%s', APP_ROOT, $p))) {
+				$stan_config[] = sprintf('		- ../%s', $p);
+			}
+		}
+		$stan_config[] = '	ignoreErrors:';
+		$stan_config[] = '		- /Undefined variable: \\\$this/';
+		$stan_config[] = '		- /Using \\\$this outside a class/';
+		$stan_config[] = '		- /Variable \\\$this might not be defined/';
+		$stan_config[] = '	scanDirectories:';
+		$stan_config[] = '		- ../vendor';
+		$stan_config[] = '	tipsOfTheDay: false';
+
+		implode("\n", $stan_config);
 
 		file_put_contents($this->config_file, $stan_config);
 
