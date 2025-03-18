536c2f17cf9473118a03547b7c397d4af58b1cca
openthc <code@openthc.com>
2025-03-18 18:39:59 +0000

    Let Exception Bubble

--- lib/Traits/JSONValidator.php
+++ lib/Traits/JSONValidator.php
@@ -18,24 +18,29 @@ trait JSONValidator
 	function validateJSON($source_json, $json_schema)
 	{
 		$jsv = Schema::import($json_schema);
-		try {
-			$res_json = $jsv->in($source_json);
-		} catch (\Exception $e) {
-			__exit_text($e->getMessage(), 500);
-		}
+		//try {
+			$res = $jsv->in($source_json);
+		//} catch (\Exception $e) {
+		//	__exit_text($e->getMessage(), 500);
+		//}
 	}
 }
 
+// $validator = new Validator();
+// $res_json = $validator->validate($source_data, $schema_spec);
+// if ( ! $res_json->isValid()) {
+// 	__exit_text($res_json->error()->__toString(), 500);
+// }
 
-		// $schema = Schema::import($schema_spec);
-		// try {
-		// 	$res_json = $schema->in($source_data);
-		// } catch (\Exception $e) {
-		// 	__exit_text($e->getMessage(), 500);
-		// }
+// $schema = Schema::import($schema_spec);
+// try {
+// 	$res_json = $schema->in($source_data);
+// } catch (\Exception $e) {
+// 	__exit_text($e->getMessage(), 500);
+// }
 
-		// $validator = new Validator();
-		// $res_json = $validator->validate($source_data, $schema_spec);
-		// if ( ! $res_json->isValid()) {
-		// 	__exit_text($res_json->error()->__toString(), 500);
-		// }
+// $validator = new Validator();
+// $res_json = $validator->validate($source_data, $schema_spec);
+// if ( ! $res_json->isValid()) {
+// 	__exit_text($res_json->error()->__toString(), 500);
+// }
